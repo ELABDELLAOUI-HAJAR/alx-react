fe6a5720b2e488dca83c911a294f0abd9d1dae8f
HAJAR <elabdellaoui.hajar@gmail.com>
2024-02-18 22:24:06 +0100

    test

--- 0x05-react_state/task_0/dashboard/config/webpack.config.js
+++ 0x05-react_state/task_0/dashboard/config/webpack.config.js
@@ -1,47 +1,43 @@
-const path = require('path')
-
+const path = require("path");
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
+    mode: "development",
+    devtool: "inline-source-map",
+    entry: "./src/index.js",
     output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        filename: "bundle.js",
+        path: path.resolve("./dist"),
+    },
+    devServer: {
+        hot: true,
+        contentBase: path.resolve("./dist"),
+        compress: true,
+        port: 8564,
     },
     module: {
         rules: [
             {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
             },
             {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(gif|png|jpe?g|svg)$/i,
                 use: [
-                    'file-loader',
+                    "file-loader",
                     {
-                        loader: 'image-webpack-loader',
+                        loader: "image-webpack-loader",
                         options: {
-                            bypassOnDebug: true,
-                            disable: true,
+                            bypassOnDebug: true, // webpack@1.x
+                            disable: true, // webpack@2.x and newer
                         },
                     },
                 ],
             },
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                },
-            },
         ],
     },
-    devServer: {
-        static: path.join(__dirname, '../dist'),
-        open: true,
-        compress: true,
-        hot: true,
-        liveReload: true,
-    },
-    devtool: 'inline-source-map',
-}
+};
