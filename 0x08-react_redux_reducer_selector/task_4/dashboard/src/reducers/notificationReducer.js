062f70ac8cf4cff0dad7ae5ddbc6d71550ca8723
HAJAR <elabdellaoui.hajar@gmail.com>
2024-03-05 14:08:31 +0100

    fix notif reducer

--- 0x08-react_redux_reducer_selector/task_4/dashboard/src/reducers/notificationReducer.js
+++ 0x08-react_redux_reducer_selector/task_4/dashboard/src/reducers/notificationReducer.js
@@ -4,17 +4,23 @@ import { notificationsNormalizer } from '../schema/notifications';
 
 export const initialState = fromJS({
   notifications: [],
-  filter: notifActions.NotificationTypeFilters,
+  filter: [],
 });
 
 export function notificationReducer(state = initialState, action = { type: null }) {
   switch (action.type) {
     case notifActions.FETCH_NOTIFICATIONS_SUCCESS:
-      const normalizData = notificationsNormalizer(action.data);
-      Object.keys(normalizData).map((key) => {
-        normalizData[key].isRead = false;
-      });
-      return state.merge(normalizData);
+      return state.merge(
+        fromJS({
+          filter: notifActions.NotificationTypeFilters.DEFAULT,
+          notifications: notificationsNormalizer(
+            action.data.map((notification) => ({
+              ...notification,
+              isRead: false,
+            }))
+          ),
+        })
+      );
 
     case notifActions.MARK_AS_READ:
       return state.setIn(['notifications', 'entities', 'notifications', action.index, 'isRead'], true);
--- 0x08-react_redux_reducer_selector/task_4/dashboard/src/reducers/notificationReducer.test.js
+++ 0x08-react_redux_reducer_selector/task_4/dashboard/src/reducers/notificationReducer.test.js
@@ -10,34 +10,27 @@ describe('Tests for notificationReducer', () => {
   });
 
   it('Test that notificationReducer returns the correct data when passing FETCH_NOTIFICATIONS_SUCCESS action', () => {
-    const data = [
-      { id: 1, type: 'default', value: 'New course available' },
-      { id: 2, type: 'urgent', value: 'New resume available' },
-      { id: 3, type: 'urgent', value: 'New data available' },
-    ];
-
-    const fetchAction = {
+    const action = {
       type: notifActions.FETCH_NOTIFICATIONS_SUCCESS,
-      data: data,
+      data: [
+        { id: 1, type: 'default', value: 'New course available' },
+        { id: 2, type: 'urgent', value: 'New resume available' },
+        { id: 3, type: 'urgent', value: 'New data available' },
+      ],
     };
 
-    const response = notificationReducer(initialState, fetchAction);
-    const expectedState = {
-      "entities":
-      {
-        "isRead": false,
-        "notifications": {
-          "1": { "id": 1, "type": "default", "value": "New course available" },
-          "2": { "id": 2, "type": "urgent", "value": "New resume available" },
-          "3": { "id": 3, "type": "urgent", "value": "New data available" }
-        }
-      },
-      "filter": { "DEFAULT": "DEFAULT", "URGENT": "URGENT" },
-      "notifications": [],
-      "result": [1, 2, 3]
-    }
+    const received_state = notificationReducer(initialState, action);
+    const expected_state = initialState.merge({
+      filter: notifActions.NotificationTypeFilters.DEFAULT,
+      notifications: notificationsNormalizer(
+        action.data.map((notification) => ({
+          ...notification,
+          isRead: false,
+        }))
+      ),
+    });
 
-    expect(response.toJS()).toEqual(expectedState);
+    expect(expected_state.toJS()).toEqual(received_state.toJS());
   });
 
   it('Test that MARK_AS_READ returns the data with the right item updated', () => {
